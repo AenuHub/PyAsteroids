6ad6fd515ef216a6ef4186f97c3639ff78361900
AenuHub <jesuskane92@gmail.com>
2024-10-10 11:11:22 +0300

    create player unit and keyboard turn function

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    frames = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+
+    # create a player object and spawn it at the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        # this is simply for 60 frames per second
+        dt = frames.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
